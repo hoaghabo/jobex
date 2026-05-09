c2e303d25ae5e818895adc92e5520f3dd3143d4a
Hoboshoon <hosseinbozorgi1382@gmail.com>
2026-05-09 19:59:17 +0330

    update allows origin

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -170,7 +170,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://app.jobex.runflare.run",
+    "https://jobexservice.ir",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
